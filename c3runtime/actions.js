497de0f57d3ea6535171b721e90d08ec54c54bba
Clément Busschaert <clement.busschaert@gmail.com>
2021-12-17 02:21:11 +0100

    [fix][typo] fix the goto action

--- c3runtime/actions.js
+++ c3runtime/actions.js
@@ -1,33 +1,37 @@
 "use strict";
 
 {
-	self.C3.Plugins.SmugRainbowPony_INK.Acts =
-	{
-		SetStory(jsonstr) {
-			this._SetStory(jsonstr);
-		},
-		LoadState(jsonstr) {
-			this._story.state.LoadJson(jsonstr)
-		},
-		StartObservingVariable(name) {
-			this._story.ObserveVariable(name, (name, new_value) => {
-				this._changedvars[name] = new_value;
-				this.Trigger(C3.Plugins.SmugRainbowPony_INK.Cnds.VariableChanged)
-			});
-		},
-		Continue(maximally) {
-			if (maximally) { this._story.ContinueMaximally(); }
-			else { this._story.Continue(); }
-		},
-		Choose(choice) {
-			this._loopedChoice = null;
-			this._story.ChooseChoiceIndex(choice);
-		},
-		Goto(path) {
-			this._story.ChoosePathString(path);
-		},
-		SetVariable(name, value) {
-			this._story.variablesState[name] = value;
-		}
-	};
+    self.C3.Plugins.SmugRainbowPony_INK.Acts = {
+        SetStory(jsonstr) {
+            this._SetStory(jsonstr);
+        },
+        LoadState(jsonstr) {
+            this._story.state.LoadJson(jsonstr);
+        },
+        StartObservingVariable(name) {
+            this._story.ObserveVariable(name, (name, new_value) => {
+                this._changedvars[name] = new_value;
+                this.Trigger(
+                    C3.Plugins.SmugRainbowPony_INK.Cnds.VariableChanged
+                );
+            });
+        },
+        Continue(maximally) {
+            if (maximally) {
+                this._story.ContinueMaximally();
+            } else {
+                this._story.Continue();
+            }
+        },
+        Choose(choice) {
+            this._loopedChoice = null;
+            this._story.ChooseChoiceIndex(choice);
+        },
+        GoTo(path) {
+            this._story.ChoosePathString(path);
+        },
+        SetVariable(name, value) {
+            this._story.variablesState[name] = value;
+        },
+    };
 }
